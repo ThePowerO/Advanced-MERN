4321ec1179da346b2024f6543af708a9e9d36190
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-13 16:26:33 -0300

    Update Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Post from './Post/Post';
 import makeStyles from './styles';
+import { useSelector } from 'react-redux';
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts)
   const classes = makeStyles();
+
+  console.log(posts)
   return (
     <>
         <h1>Posts</h1>

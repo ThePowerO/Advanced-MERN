4ab52d6154c7fe97dbdbf7b62c25e0131e1128e6
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-13 16:25:12 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Advanced-MERNdb:RD4uYGjbSvPyplNt@cluster0.lja4ilt.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
@@ -19,5 +19,3 @@ const PORT = process.env.PORT|| 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set();
